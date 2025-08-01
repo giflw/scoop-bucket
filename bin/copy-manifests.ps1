59453ce798858a755628542b7cebde2c5bae1cdc
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2025-08-01 01:55:17 -0300

    Update apps from original buckets

--- bin/copy-manifests.ps1
+++ bin/copy-manifests.ps1
@@ -4,9 +4,9 @@ Write-Output "Workspace: ${workspace}"
 $scoopPrefix = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent "$(scoop prefix *)"))
 Write-Output "Scoop Prefix: ${scoopPrefix}"
 
-Write-Output "====================================="
-Write-Output "Copying installed apps even if exists"
-Write-Output "====================================="
+Write-Output "=========================================="
+Write-Output "Copying installed manifests even if exists"
+Write-Output "=========================================="
 
 foreach ($app in (scoop list 2>$null 3>$null 4>$null 5>$null 6>$null)) {
     $name = "$($app.Name)"
@@ -24,26 +24,20 @@ foreach ($app in (scoop list 2>$null 3>$null 4>$null 5>$null 6>$null)) {
 }
 
 
-#Write-Output "========================================"
-#Write-Output "Copying apps from buckets if exists here"
-#Write-Output "========================================"
+Write-Output "============================================="
+Write-Output "Copying manifests from buckets if exists here"
+Write-Output "============================================="
 
-#foreach($bucket in (scoop bucket list 2>$null 3>$null 4>$null 5>$null 6>$null)) {
-#foreach($app in (scoop list 2>$null 3>$null 4>$null 5>$null 6>$null)) {
-#$name = "$($app.Name)"
-#Write-Output "Installed app: ${bucket}/${name}"
-#if (Test-Path -Path "${workspace}\bucket\${name}.json") {
-#Write-Output "    Manifest exists."
-#} else {
-#Write-Output "    Copying manifest..."
-#$manifest = "${scoopPrefix}\buckets\${bucket}\bucket\${name}.json"
-#if (Test-Path -Path "${manifest}") {
-#Copy-Item -Path "${manifest}" -Destination "${workspace}\bucket\${app.json}"
-#} else {
-#Write-Output "    Error: manifest file not found!"
-#Write-Output "        ${manifest}"
-#Write-Output "        Name: '${name}'    Bucket: '${bucket}'"
-#}
-#}
-#}
-#}
+$bucketsDir = (Resolve-Path "$(scoop prefix scoop)/../../../buckets")
+foreach ($app in (Get-ChildItem "$workspace/bucket")) {
+    foreach ($bucket in ("main", "extras", "versions", "nerd-fonts")) {
+        $name = "$($app.Name.Split(".")[0])"
+        Write-Output "Bucketed manifest: ${bucket}/${name}"
+        $manifest = "${bucketsDir}\${bucket}\bucket\${name}.json"
+        #Write-Output "    ${manifest}"
+        if (Test-Path -Path "${manifest}") {
+            Write-Output "    Copying manifest..."
+            Copy-Item -Path "${manifest}" -Destination "${workspace}\bucket\${name}.json"
+        }
+    }
+}
